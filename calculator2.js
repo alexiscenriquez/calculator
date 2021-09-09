dd61d6e0fbb00a21de2b7c85bc56d42e5ae344a9
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-08 20:02:50 -0500

    progress

--- calculator2.js
+++ calculator2.js
@@ -3,24 +3,36 @@ let screen = document.getElementById("one");
 let clear = document.getElementById("two");
 let output = document.querySelectorAll("#one")
 let toEvaluate
-let num
+let num = 0
+let calculation
+let evaluate = document.getElementById("equals");
 for (let i = 0; i < val.length; i++) {
     val[i].addEventListener("click", () => {
         console.log(val[i].value)
         screen.textContent += val[i].value;
         toEvaluate += val[i].value;
+
+        clear.addEventListener("click", () => {
+            toEvaluate = 0;
+            screen.textContent = ""
+            return toEvaluate;
+        })
+        return toEvaluate
     })
 }
-clear.addEventListener("click", () => {
-    screen.textContent = "";
-})
 
-let evaluate = document.getElementById("equals");
+
 evaluate.addEventListener("click", () => {
     num = toEvaluate.slice(9)
-    screen.textContent = eval(num);
-    if (screen.length >= 8) {
-        Math.ceil(screen.value)
-    }
+    calculation = eval(num)
+    screen.textContent = calculation;
+    return calculation;
+
 }
 )
+
+/*function clr() {
+    toEvaluate = 0;
+    screen.textContent = ""
+    return toEvaluate;
+}*/
